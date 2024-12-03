f6c720404960e7882358eeb384f28d2feb8b38a0
HarryInsa <harry.kalfon@gmail.com>
2024-12-03 15:13:23 +0100

    Refactor LFUCache to support byte capacity and persistent frequency storage

--- server.py
+++ server.py
@@ -1,42 +1,78 @@
+import os
+import json
 import requests
 from flask import Flask, Response
 from collections import OrderedDict
 
 class LFUCache:
-    def __init__(self, capacity=3):
-        self.capacity = capacity
+    def __init__(self, capacity_bytes=5 * 1024 * 1024, cache_dir='cache'):
+        self.capacity_bytes = capacity_bytes
+        self.current_size = 0
+        self.cache_dir = cache_dir
+        self.frequency_file = os.path.join(cache_dir, 'frequency.json')
         self.cache = OrderedDict()
         self.frequency = {}
+
+        if not os.path.exists(cache_dir):
+            os.makedirs(cache_dir)
+
+        if os.path.exists(self.frequency_file):
+            with open(self.frequency_file, 'r') as f:
+                self.frequency = json.load(f)
+
+        for filename in os.listdir(cache_dir):
+            if filename != 'frequency.json':
+                file_path = os.path.join(cache_dir, filename)
+                self.cache[filename] = file_path
+                self.current_size += os.path.getsize(file_path)
+
+    def save_frequency(self):
+        with open(self.frequency_file, 'w') as f:
+            json.dump(self.frequency, f)
 
     def get(self, key):
         if key not in self.cache:
             return None
 
         self.frequency[key] += 1
+        self.save_frequency()
         
-        value = self.cache[key]
-        del self.cache[key]
+        file_path = self.cache[key]
+        with open(file_path, 'rb') as f:
+            data = f.read()
+
+        value = self.cache.pop(key)
         self.cache[key] = value
-        return value
+        return data
 
     def put(self, key, value):
+        file_size = len(value)
         if key in self.cache:
-            self.cache[key] = value
             self.frequency[key] += 1
+            self.save_frequency()
             return
 
-        if len(self.cache) >= self.capacity:
+        while self.current_size + file_size > self.capacity_bytes:
             lfu_key = min(self.frequency, key=self.frequency.get)
+            lfu_file_path = self.cache[lfu_key]
+            self.current_size -= os.path.getsize(lfu_file_path)
+            os.remove(lfu_file_path)
             del self.cache[lfu_key]
             del self.frequency[lfu_key]
 
-        self.cache[key] = value
+        file_path = os.path.join(self.cache_dir, key)
+        with open(file_path, 'wb') as f:
+            f.write(value)
+
+        self.cache[key] = file_path
         self.frequency[key] = 1
+        self.current_size += file_size
+        self.save_frequency()
 
 app = Flask(__name__)
-ORIGIN_SERVER = 'http://3.3.3.3:8000' 
+ORIGIN_SERVER = 'http://localhost:8000' 
 
-image_cache = LFUCache(capacity=3)
+image_cache = LFUCache(capacity_bytes=10 * 1024 * 1024)  
 
 @app.route('/<image_name>', methods=['GET'])
 def get_image(image_name):
@@ -54,4 +90,4 @@ def get_image(image_name):
     return 'Image not found', 404
 
 if __name__ == '__main__':
-    app.run(host='2.2.2.2', port=5000)
+    app.run(host='0.0.0.0', port=5000)
